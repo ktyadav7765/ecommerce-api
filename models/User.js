1df5e2d98ac3cfa87d47a8b7c6cd2dd044aaf1c2
ktyadav7765 <ktyadav7765@gmail.com>
2026-05-03 11:48:35 +0530

    modified User.js

--- models/User.js
+++ models/User.js
@@ -56,7 +56,7 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function () {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
 });
